88264c34b204ba72a45407e0a08d397a2e7e63a8
nøcomputer <wim@nocomputer.be>
2018-04-26 11:40:43 +0200

    added publish/receive video example

---
+++ videoPublishReceive/src/ofApp.cpp
@@ -0,0 +1,41 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	videopublisher.bind("tcp://*:14000");
+	//videopublisher.setHighWaterMark(2);
+	//videopublisher.setSendHighWaterMark(2);
+	videoreceiver.connect("tcp://localhost:14000");
+	//videoreceiver.setReceiveHighWaterMark(2);
+
+	camera.setup(1280, 720);
+	
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+	camera.update();
+
+	if (camera.isFrameNew()) {
+		videopublisher.send(camera.getPixels().getData(), 3*1280*720* sizeof(unsigned char));
+	}
+
+	while (videoreceiver.hasWaitingMessage())
+	{
+		ofBuffer data;
+		videoreceiver.getNextMessage(data);
+		image.setFromPixels((unsigned char*)data.getData(), 1280, 720,OF_IMAGE_COLOR,true);
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+	if (image.isAllocated()) {
+		image.draw(0, 0);
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
---
+++ videoPublishReceive/src/ofApp.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxZmq.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		ofxZmqPublisher videopublisher;
+		ofxZmqSubscriber videoreceiver;
+
+		ofVideoGrabber camera;
+		
+		ofImage image;
+		
+};
